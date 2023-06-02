2b287bc1dca7d5ecade944e4f77e7fd086403ae4
Cássio Vagner dos Santos Diogo <cassiovsd@hotmail.com>
2023-06-02 13:40:45 -0300

    adicionado imagens certas

--- interaction.js
+++ interaction.js
@@ -2,43 +2,43 @@ let ITENS_LOJA = [
     {
         nome:"Bola da Copa",
         preco:86.30,
-        url_img:"./img/bola_copa.webp",
+        url_img:"./img/bola da copa.png",
         descricao:"Bola utilizada na Copa do Mundo, idela para utilizar em campo e colecionar"
     },
     {
         nome:"Carrinho da Hot Wheels",
         preco:5.80,
-        url_img:"./img/carro_hotwheels.jpeg",
+        url_img:"./img/carrinho.png",
         descricao:"Carrinho para dar de presentes para crianças ou colecionar"
     },
     {
         nome:"Chinelo",
         preco:19.99,
-        url_img:"./img/chinelo.webp",
+        url_img:"./img/chinelo.png",
         descricao:"Chinelo para se utilizar em qualquer lugar"
     },
     {
         nome:"Escorredor de Louça",
         preco:15.20,
-        url_img:"./img/escorredor_louca.webp",
+        url_img:"./img/escorredor.jpg",
         descricao:"Utilizando para ajudar no processo de secagem dos utensilios"
     },
     {
         nome:"Espada Medieval",
         preco:200.18,
-        url_img:"./img/espada_medieval.png",
+        url_img:"./img/espada.png",
         descricao:"Utilizada em batalhar para neutralizar o inimigo ou como item de decoração"
     },
     {
         nome:"Guitarra",
         preco:1980.99,
-        url_img:"./img/guitarra.webp",
+        url_img:"./img/guitarra.png",
         descricao:"Otima para utilizar em bandas de hard rock"
     },
     {
         nome:"Toalha de Banho",
         preco:9.99,
-        url_img:"./img/toalha_banho.webp",
+        url_img:"./img/toalha.png",
         descricao:"Boa para se secar apos o banho"
     },
     {
@@ -57,12 +57,12 @@ function init() {
         `
         <div class="item">
             <div class="img-bg">
-                <img class="item-img" src="./img/Dados.png" alt="Imagem do Produto">
+                <img class="item-img" src="${item.url_img}" alt="Imagem do Produto">
             </div>
 
             <h1 class="item-name">${item.nome}</h1>
             <h3 class="item-desc">${item.descricao}</h3>
-            <h2 class="item-price"> R$ ${item.preco}</h2>
+            <h2 class="item-price">${item.preco.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</h2>
         </div>
         `;
     }) 
