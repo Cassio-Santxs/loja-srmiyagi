4cd7a7f7d76f37e6ba780ac63413885bc258d792
Cássio Vagner dos Santos Diogo <cassiovsd@hotmail.com>
2023-06-01 21:48:09 -0300

    Add files via upload

---
+++ interaction.js
@@ -0,0 +1,79 @@
+let ITENS_LOJA = [
+    {
+        nome:"Bola da Copa",
+        preco:86.30,
+        url_img:"./img/bola_copa.webp",
+        descricao:"Bola utilizada na Copa do Mundo, idela para utilizar em campo e colecionar"
+    },
+    {
+        nome:"Carrinho da Hot Wheels",
+        preco:5.80,
+        url_img:"./img/carro_hotwheels.jpeg",
+        descricao:"Carrinho para dar de presentes para crianças ou colecionar"
+    },
+    {
+        nome:"Chinelo",
+        preco:19.99,
+        url_img:"./img/chinelo.webp",
+        descricao:"Chinelo para se utilizar em qualquer lugar"
+    },
+    {
+        nome:"Escorredor de Louça",
+        preco:15.20,
+        url_img:"./img/escorredor_louca.webp",
+        descricao:"Utilizando para ajudar no processo de secagem dos utensilios"
+    },
+    {
+        nome:"Espada Medieval",
+        preco:200.18,
+        url_img:"./img/espada_medieval.png",
+        descricao:"Utilizada em batalhar para neutralizar o inimigo ou como item de decoração"
+    },
+    {
+        nome:"Guitarra",
+        preco:1980.99,
+        url_img:"./img/guitarra.webp",
+        descricao:"Otima para utilizar em bandas de hard rock"
+    },
+    {
+        nome:"Toalha de Banho",
+        preco:9.99,
+        url_img:"./img/toalha_banho.webp",
+        descricao:"Boa para se secar apos o banho"
+    },
+    {
+        nome:"Vinho",
+        preco:59.90,
+        url_img:"./img/vinho.webp",
+        descricao:"Vinho que harmoniza muito bem com carnes vermelhas "
+    }
+]
+
+function init() {
+    let itensSection = document.getElementById("itens_section");
+
+    ITENS_LOJA.forEach(item => {
+        let itenTitle = document.createElement("div");
+
+        itenTitle.classList.add("item");
+
+        itenTitle.innerHTML = 
+        `
+        <div class="item">
+            <div class="img-bg">
+                <img class="item-img" src="./img/Dados.png" alt="Imagem do Produto">
+            </div>
+
+            <h1 class="item-name">${item.nome}</h1>
+            <h3 class="item-desc">${item.descricao}</h3>
+            <h2 class="item-price"> R$ ${item.preco}</h2>
+        </div>
+        `;
+
+        itensSection.appendChild(itenTitle);
+    }) 
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    init();
+});
